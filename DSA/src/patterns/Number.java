cbecf52275eab6d0cbe6d01b5e34b4c67ee88740
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-01 07:19:40 +0530

    Number increasing triangle

--- DSA/src/patterns/Number.java
+++ DSA/src/patterns/Number.java
@@ -4,7 +4,16 @@ public class Number {
 
 	public static void main(String[] args)
 	{
-		
+		int n=4;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(i>=j) System.out.print("1 ");
+				else System.out.print("  ");
+			}
+			System.out.println();
+		}
 	}
 
 }
