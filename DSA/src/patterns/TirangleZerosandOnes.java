072145c31bfece81693931d3d674f5cf9246fb64
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-04 07:21:19 +0530

    Patterns

---
+++ DSA/src/patterns/TirangleZerosandOnes.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class TirangleZerosandOnes {
+
+	public static void main(String[] args)
+	{
+		int n=6;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(i>=j)
+				{
+				if((i+j)%2==0) System.out.print(1+ " ");
+				else System.out.print(0 +" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
