c78ae3132b383b81d37c9ebc38f29c1470f45f91
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-04 07:00:14 +0530

    Increasing Number

---
+++ DSA/src/patterns/NumberIncreasingReversePyramidPattern.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class NumberIncreasingReversePyramidPattern {
+
+	public static void main(String[] args)
+	{
+		int n=6;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(i+j<=n+1) System.out.print(j+" ");
+				else System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
