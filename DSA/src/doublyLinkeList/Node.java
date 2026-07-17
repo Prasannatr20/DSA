84b77e0d0f72fdfa2cd8ba1d5d32adc1907dd7b6
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-17 07:28:04 +0530

    Added Constructors

--- DSA/src/doublyLinkeList/Node.java
+++ DSA/src/doublyLinkeList/Node.java
@@ -5,4 +5,23 @@ public class Node
 	int data;
 	Node next;
 	Node prev;
+	Node()
+	{
+		
+	}
+	Node(int data)
+	{
+		this.data=data;
+	}
+	Node(int data, Node next)
+	{
+		this.data=data;
+		this.next=next;
+	}
+	Node(int data, Node next, Node prev)
+	{
+		this.data=data;
+		this.next=next;
+		this.prev=prev;
+	}
 }
