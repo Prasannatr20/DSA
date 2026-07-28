1ec6775d3549e4146dabd8467e1aa2aa462c0af9
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-28 07:04:46 +0530

    Optimized

--- DSA/src/math/PrintAllDivisors.java
+++ DSA/src/math/PrintAllDivisors.java
@@ -1,6 +1,7 @@
 package math;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrintAllDivisors {
 
@@ -8,13 +9,32 @@ public class PrintAllDivisors {
 	{
 		System.out.println(divisor(36));
 	}
+	//BruteForce
+//	static ArrayList<Integer> divisor(int n)
+//	{
+//		ArrayList<Integer> list = new ArrayList<>();
+//		for(int i=1;i<=n;i++)
+//		{
+//			if(n%i==0) list.add(i);
+//		}
+//		return list;
+//	}
 	static ArrayList<Integer> divisor(int n)
 	{
-		ArrayList<Integer> list = new ArrayList<>();
-		for(int i=1;i<=n;i++)
+		ArrayList<Integer> list= new ArrayList<>();
+		for(int i=1;i*i<=n;i++)
 		{
-			if(n%i==0) list.add(i);
+			if(n%i==0)
+			{
+				if(i*i==n) list.add(i);
+				else
+				{
+					list.add(i);
+					list.add(n/i);
+				}
+			}
 		}
+		Collections.sort(list);
 		return list;
 	}
 }
