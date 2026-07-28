ab4b19e961bf500813b02dc530f79b2a4858080d
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-28 22:49:33 +0530

    Commit

---
+++ DSA/src/recursionn/OneToN.java
@@ -0,0 +1,15 @@
+package recursionn;
+
+public class OneToN {
+
+	public static void main(String[] args)
+	{
+		numbers(10);
+	}
+	static void numbers(int n)
+	{
+	  	if(n==0) return;
+		numbers(n-1);
+		System.out.println(n);
+	}
+}
