32389f2aa155c67ab7fe8a44d92c873dd2202c97
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-30 07:05:34 +0530

    Commit

--- DSA/src/recursionn/StackHeight.java
+++ DSA/src/recursionn/StackHeight.java
@@ -2,7 +2,7 @@ package recursionn;
 
 public class StackHeight {
 
-	//Height of stack in x^n. Stack height is always n-1
+	//Height of stack in x^n. Stack height is always n
 	public static void main(String[] args)
 	{
 		System.out.println(height(3, 8));
