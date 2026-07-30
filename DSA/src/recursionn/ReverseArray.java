aa0c44b8b03d7636a8d23725a9e475cd756db943
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-30 07:34:40 +0530

    Reverse Array

---
+++ DSA/src/recursionn/ReverseArray.java
@@ -0,0 +1,22 @@
+package recursionn;
+
+public class ReverseArray {
+
+	public static void main(String[] args)
+	{
+		int[] arr= {1,2,3,4,5};
+		reverse(arr, 0, arr.length-1);
+		for(int a: arr)
+		{
+			System.out.print(a+" ");
+		}
+	}
+	static void reverse(int[] arr, int start, int end)
+	{
+		if(start>= end) return;
+		int temp=arr[start];
+		arr[start]= arr[end];
+		arr[end]=temp;
+		reverse(arr, start+1, end-1);
+	}
+}
