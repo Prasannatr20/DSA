aa38f30d113a4635e58fbc166ac95661763e9d24
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-07-30 07:01:36 +0530

    Stack Height

---
+++ DSA/src/recursionn/StackHeight.java
@@ -0,0 +1,19 @@
+package recursionn;
+
+public class StackHeight {
+
+	//Height of stack in x^n. Stack height is always n-1
+	public static void main(String[] args)
+	{
+		System.out.println(height(3, 8));
+	}
+	static int height(int x, int n)
+	{
+		if(x==0 || x==1) return 1;
+		if(n==0) return 1;
+		int a= height(x, n-1);
+		a=x*a;
+		return a;
+	}
+
+}
