75dd67b2800007a3e4cfaed7160e32b65a62b047
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-08-18 07:58:34 +0530

    Added methods

--- DSA/src/hashTable/SupermartProject.java
+++ DSA/src/hashTable/SupermartProject.java
@@ -5,22 +5,100 @@ import java.util.Scanner;
 
 public class SupermartProject
 {
-	static Scanner sc= new Scanner(System.in);
+	static Scanner scan= new Scanner(System.in);
+	static Hashtable<String, Integer> t= new Hashtable<>();
 	public static void main(String[] args)
 	{
-		
+		System.out.println("Actions that you can perform");
+		System.out.println("Press for: \n"
+				+ "1. Add Fruit and quantity\n"
+				+ "2. Remove a fruit\n"
+				+ "3. Display Friut and respective quantity\n"
+				+ "4. See if a fruit is present\n"
+				+ "5. Total no.of fruits\n"
+				+ "6. Show all fruits\n"
+				+ "7. Clear all fruits\n"
+				+ "0. Exit");
+		int action;
+		boolean val=true;
+		while(val)
+		{
+			action= scan.nextInt();
+			scan.nextLine();
+			if(action==0) val=false;
+			switch(action)
+			{
+				case 1:
+					System.out.println("Enter fruit and quantity");
+					put(scan.next(), scan.nextInt());
+				break;
+				case 2:
+					System.out.println("Enter fruit");
+					remove(scan.next());
+				break;
+				case 3: show();
+				break;
+				case 4: present(scan.next());
+				break;
+				case 5: size();
+				break;
+				case 6: showAllFruits();
+				break;
+				case 7: clear();
+				break;
+				default: System.out.println("Enter a valid number");
+			}
+		}
 	}
 	
-	static Hashtable<String, Integer> put(String fruit, Integer quantity)
+	static void put(String fruit, Integer quantity)
 	{
-		Hashtable<String, Integer> t= new Hashtable<>();
 		t.put(fruit, quantity);
-		return t;
+		System.out.println("Added");
 	}
-	static Hashtable<String, Integer> remove(String fruit, Integer quantity)
+	static void remove(String fruit)
 	{
-		Hashtable<String, Integer> t= new Hashtable<>();
-		t.remove(fruit);
-		return t;
+		if(t.contains(fruit))
+		{
+			t.remove(fruit);
+		}
+		else
+		{
+			System.out.println("The fruit you entered is not available/ Enter correct spelling");
+			return;
+		}
+		System.out.println("Removed");
+	}
+	static void show()
+	{
+		for(String i:t.keySet())
+		{
+			System.out.println(i+"  : "+ t.get(i));
+		}
+		System.out.println("End of list");
+	}
+	static void present(String fruit)
+	{
+		if(t.contains(fruit))
+		{
+			System.out.println(fruit +" "+ "is available  Qty is: "+ t.get(fruit));
+		}
+		else
+		{
+			System.out.println(fruit +" "+ "is not available");
+		}
+	}
+	static void size()
+	{
+		System.out.println(t.size());
+	}
+	static void showAllFruits()
+	{
+		System.out.println(t.values());
+	}
+	static void clear()
+	{
+		t.clear();
+		System.out.println("Cleared");
 	}
 }
